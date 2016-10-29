6cb6b1a9d446ef6166f879cb2de7790bdfccfa2d
ultinomics <ultinomics@github.com>
2016-10-29 00:23:43 -0400

    render test

--- figures/transform.sh
+++ figures/transform.sh
@@ -1,7 +1,7 @@
 rmtrash expansion-v* expansion-h*
 find . -name '*svg' | xargs -I @ basename -s .svg @ | xargs -I @ convert @.svg @.png
 mogrify -shave 10x10 -bordercolor black -border 10  *.png
-convert *.png -append expansion-v.pdf
-convert *.png -append expansion-h.pdf
-convert *.png +append expansion-h.png
-convert *.png +append expansion-v.png
+convert pub-expansion*.png -append expansion-v.pdf
+convert pub-expansion*.png +append expansion-h.pdf
+convert pub-expansion*.png -append expansion-v.png
+convert pub-expansion*.png +append expansion-h.png
