06adb380e346ca8c3e4d85a86032792f2561ff7f
ultinomics <ultinomics@github.com>
2016-10-31 14:08:05 -0400

    starting methods section

--- helpers.R
+++ helpers.R
@@ -1,3 +1,14 @@
+# packages
+library(tidyverse)
+library(xtable)
+devtools::load_all('~/GitHub/spartannash')
+
+# functions
 getOutputFormat <- function() {
   knitr::opts_knit$get("rmarkdown.pandoc.to")
+}
+
+printTable <- function(tbl, format = getOutputFormat()) {
+    cap <- attr(tbl, "caption")
+    if(format == 'latex') print(tbl, type = format, comment = FALSE) else knitr::kable(tbl, caption=cap)
 }
