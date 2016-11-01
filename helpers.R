8baa79b30d3bfb9180a06b5bd120075dd3110a62
ultinomics <ultinomics@github.com>
2016-11-01 01:26:03 -0400

    more words. removed some files

--- helpers.R
+++ helpers.R
@@ -1,6 +1,7 @@
 # packages
 library(tidyverse)
 library(xtable)
+library(extrafont)
 devtools::load_all('~/GitHub/spartannash')
 
 # functions
@@ -10,5 +11,22 @@ getOutputFormat <- function() {
 
 printTable <- function(tbl, format = getOutputFormat()) {
     cap <- attr(tbl, "caption")
-    if(format == 'latex') print(tbl, type = format, comment = FALSE) else knitr::kable(tbl, caption=cap)
+    if(format == 'latex') print(tbl, type = format, comment = FALSE, include.rownames=FALSE) else knitr::kable(tbl, caption=cap)
+}
+
+# ggplot themes
+theme_clean <- function(base_size=12, base_family="Source Sans Pro Light") {
+  ret <- theme_bw(base_size, base_family) +
+    theme(panel.background = element_rect(fill="#ffffff", colour=NA),
+          axis.title.x=element_text(vjust=-0.2), axis.title.y=element_text(vjust=1.5),
+          title=element_text(vjust=1.2, family="Source Sans Pro Semibold"),
+          axis.line=element_line(size = 3, linetype = 1, colour = 'black'),
+          panel.grid=element_blank(), axis.ticks=element_blank(),
+          legend.position="bottom",
+          axis.title=element_text(size=rel(0.8), family="Source Sans Pro Semibold"),
+          strip.text=element_text(size=rel(1), family="Source Sans Pro Semibold"),
+          strip.background=element_rect(fill="#ffffff", colour=NA),
+          panel.margin.y=unit(1.5, "lines"))
+
+  ret
 }
